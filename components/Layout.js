9d0e8b8e32ea377ec2ebed0cc488e4e8998bf704
mhkmcp <mhkmcp@yahoo.com>
2021-11-25 01:04:07 +0600

    added styles on layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@mui/material';
 
+import useStyles from '../utils/styles';
+
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next-Amazona</title>
-        <AppBar position="static">
+        <AppBar position="static" className={classes.navbar}>
           <Toolbar>
             <Typography>amazona</Typography>
           </Toolbar>
